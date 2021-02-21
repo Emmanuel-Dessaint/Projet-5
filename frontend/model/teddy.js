f8b739c4dc635003f19fca4cfe6e140a126b96a4
Emmanuel-Dessaint <73949384+Emmanuel-Dessaint@users.noreply.github.com>
2021-02-21 15:17:43 +0100

    first commit

---
+++ frontend/model/teddy.js
@@ -0,0 +1,11 @@
+class Teddy {
+    constructor(id, name, price, description, imageUrl, color, quantity) {
+        this.id = id
+        this.name = name
+        this.price = price
+        this.description = description
+        this.imageUrl = imageUrl
+        this.color = color
+        this.quantity = quantity
+    }
+}
---
+++ frontend/pages/order/order.js
@@ -0,0 +1,15 @@
+if (window.localStorage.getItem('orderId') != null) {
+    var payed = JSON.parse(localStorage.getItem('totalPrice'))
+    var myOrderId = JSON.parse(localStorage.getItem('orderId'))
+    
+    document.getElementById('recap').innerHTML += 
+    '<h3>Merci pour votre commande!</h3>' +
+    '<p>Voici votre numéro de commande : <span id="orderId">' + myOrderId.orderId + '</span></p>' +
+    '<p>Nous préparons votre commande. Vous pourrez suivre votre commande avec ce numéro.</p><br>' +
+    '<p>Total payé : <span class="totalPayed">' + payed + ' €</span></p>'
+    
+    window.localStorage.clear()
+}
+else {
+    document.getElementById('recap').innerHTML += 'Aucune commande trouvée'
+}
